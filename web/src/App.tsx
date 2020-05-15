2b7a9eff7cae2274e5e1d08c3914dd8802c1a081
miles <miles.yan114@gmail.com>
2020-05-15 11:47:49 +0800

    setup antd and react app rewired

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Routes from "./routes";
+import { Button } from 'antd';
+
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">
+                <Button type='primary'>Home</Button>
+              </Link>
             </li>
             <li>
               <Link to="/about">About</Link>
